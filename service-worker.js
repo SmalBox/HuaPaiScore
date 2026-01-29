0e899518ab5d658b1ea48242e9372c599c8285d1
SmalBox <505414969@qq.com>
2026-01-29 17:39:47 +0800

    update:更新服务版本

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1';
-const DATA_CACHE_NAME = 'data-cache-v1';
+const CACHE_NAME = 'static-cache-v0.2.3';
+const DATA_CACHE_NAME = 'data-cache-v0.2.3';
 
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
