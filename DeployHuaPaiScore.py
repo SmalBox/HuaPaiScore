bb68b37a64da19e7eecbcf8e72ff8804905cb72e
SmalBox <505414969@qq.com>
2026-01-29 17:41:33 +0800

    fix:更新服务的部署路径斜杠

--- DeployHuaPaiScore.py
+++ DeployHuaPaiScore.py
@@ -5,13 +5,13 @@ import io
 
 print("{:=^50}".format("Start"))
 
-with io.open(".\\scripts\\app.js", 'r', encoding="utf-8") as f:
+with io.open("./scripts/app.js", 'r', encoding="utf-8") as f:
   fileContent = f.read()
   print("{:^50}".format("Read Completed"))
   fileContent = fileContent.replace("../service-worker.js", "/HuaPaiScore/service-worker.js")
   print("{:^50}".format("Replace Completed"))
 
-with io.open(".\\scripts\\app.js", 'w+', encoding="utf-8") as f:
+with io.open("./scripts/app.js", 'w+', encoding="utf-8") as f:
   f.write(fileContent)
   print("{:^50}".format("Write Completed"))
 
